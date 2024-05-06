37b37385e1a8a85d02734b2f3e609a48a68284ec
mirzayogy <mirza.yogy@gmail.com>
2024-05-06 11:27:10 +0800

    Aksi

---
+++ Aksi.java
@@ -0,0 +1,14 @@
+public class Aksi {
+    public static void main(String[] args) {
+
+        //Class object = new Constructor()
+        Mahasiswa mirza = new Mahasiswa();
+
+        mirza.nama = "Mirza";
+        mirza.npm = "001";
+        mirza.jumlahSks = 144;
+        mirza.ipk = 3.89;
+        
+        mirza.tampilkanInfo();
+    }
+}
